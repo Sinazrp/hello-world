9329f10993f8b7e899f942da4362bb6184d0e404
Sina Zerehpoosh <75861475+Sinazrp@users.noreply.github.com>
2024-07-02 20:31:17 +0330

    slices

--- main.go
+++ main.go
@@ -1,55 +1,64 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"time"
-)
-
-type User struct {
-	FirstName   string
-	LastName    string
-	phoneNumber string
-	age         int
-	BirthDate   time.Time
-}
-
-func (m *User) printUserName() {
-	log.Println("this is the call back of  func : ", m.FirstName)
-
-}
+//type User struct {
+//	FirstName   string
+//	LastName    string
+//	phoneNumber string
+//	age         int
+//	BirthDate   time.Time
+//}
+//
+//func (m *User) printUserName() {
+//	log.Println("this is the call back of  func : ", m.FirstName)
+//
+//}
 
 func main() {
+	
+	//myMap := make(map[string]string)
+	//myMap["wife"] = "Elnaz"
+	//log.Println(myMap["wife"])
+	//var mySlice []string
+	//mySlice = append(mySlice, "sina")
+	//mySlice = append(mySlice, "Elnaz", "ali")
+	//mySlice = append(mySlice, "habib")
+	//log.Println(mySlice)
+	//log.Println(myMap["wife"])
+	//numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	//log.Println(numbers)
+	//log.Println(numbers[:3])
+	//names := [4]string{"one", "two", "three", "four"}
+	//log.Println(names)
 
-	user1 := User{age: 15, LastName: "zp", FirstName: "sina", BirthDate: time.Now()}
-	user1.printUserName()
-	log.Println(user1)
-	fmt.Println("Hello, World!")
-
-	whatToSay := "Hello, World!"
-	fmt.Println(whatToSay)
-
-	i := 0
-
-	fmt.Println("i is set to", i)
-	a, b := saySomething()
-
-	fmt.Println(a, b)
-
-	myString := "green"
-	log.Println("my Satring is ", myString)
-
-	changeUsingpointer(&myString)
-	log.Println("my Satring is ", myString)
+	//user1 := User{age: 15, LastName: "zp", FirstName: "sina", BirthDate: time.Now()}
+	//user1.printUserName()
+	//log.Println(user1)
+	//fmt.Println("Hello, World!")
+	//
+	//whatToSay := "Hello, World!"
+	//fmt.Println(whatToSay)
+	//
+	//i := 0
+	//
+	//fmt.Println("i is set to", i)
+	//a, b := saySomething()
+	//
+	//fmt.Println(a, b)
+	//
+	//myString := "green"
+	//log.Println("my Satring is ", myString)
+	//
+	//changeUsingpointer(&myString)
+	//log.Println("my Satring is ", myString)
 
 }
 
-func saySomething() (string, int) {
-	return "SomeThing", 2
-}
-
-func changeUsingpointer(s *string) {
-	log.Print("my Satring  set to ", s)
-
-	*s = "changed"
-}
+//func saySomething() (string, int) {
+//	return "SomeThing", 2
+//}
+//
+//func changeUsingpointer(s *string) {
+//	log.Print("my Satring  set to ", s)
+//
+//	*s = "changed"
+//}
