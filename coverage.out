1f87b01cf740b9178356d2bba5322317efcc366d
Sina Zerehpoosh <75861475+Sinazrp@users.noreply.github.com>
2024-07-05 00:12:10 +0330

    testing second way

---
+++ coverage.out
@@ -0,0 +1,7 @@
+mode: set
+github.com/Sinazrp/mytestingpack/main.go:40.13,106.16 2 0
+github.com/Sinazrp/mytestingpack/main.go:106.16,109.3 2 0
+github.com/Sinazrp/mytestingpack/main.go:110.2,110.21 1 0
+github.com/Sinazrp/mytestingpack/main.go:113.44,115.12 2 1
+github.com/Sinazrp/mytestingpack/main.go:115.12,117.3 1 1
+github.com/Sinazrp/mytestingpack/main.go:118.2,119.20 2 1
--- main_test.go
+++ main_test.go
@@ -2,18 +2,46 @@ package main
 
 import "testing"
 
-func TestDivide(t *testing.T) {
-	_, err := divide(10.0, 1.0)
-	if err != nil {
-		t.Error("got error when we should not", err)
+var tests = []struct {
+	name     string
+	dividend float32
+	divisor  float32
+	expected float32
+	isError  bool
+}{
+	{"valid-data", 100.0, 10, 10, false},
+	{"invalid-data", -100.0, 0.0, 0.0, true},
+	{"invalid-data", 50.0, 10.0, 5.0, false},
+	{"invalid-data", -1.0, -777, 0.0012870013, false},
+}
+
+func TestDivision(t *testing.T) {
+	for _, tt := range tests {
+		got, err := divide(tt.dividend, tt.divisor)
+		if tt.isError && err == nil {
+			t.Errorf("%s: expected error", tt.name)
+		}
+		if !tt.isError && err != nil {
+			t.Errorf("%s: expected no error, got %v", tt.name, err)
+		}
+		if got != tt.expected {
+			t.Errorf("%s: expected %v, got %v", tt.name, tt.expected, got)
+		}
+
 	}
 
 }
 
-func TestDivide2(t *testing.T) {
-	_, err := divide(10.0, 0)
-	if err == nil {
-		t.Error("not got error when we should", err)
-	}
-
-}
+//func TestDivide(t *testing.T) {
+//	_, err := divide(10.0, 1.0)
+//	if err != nil {
+//		t.Error("got error when we should not", err)
+//	}
+//
+//}
+//
+//func TestDivide2(t *testing.T) {
+//	_, err := divide(10.0, 0)
+//	if err == nil {
+//		t.Error("not got error when we should", err)
+//	}
